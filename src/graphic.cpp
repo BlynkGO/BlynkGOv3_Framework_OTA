fc4c6e77e5230cb0549c920017b1814796998055
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2024-04-03 16:02:12 +0700

    update FrameWork OTA
    - ในตัวอย่าง จะเปลี่ยนข้อความให้อัตโนมัติ หาก upload ทางสาย USB ขึ้นข้อความนึง หาก upload ทาง OTA ขึ้นอีกข้อความนึง
    
    - เพิ่มคำอธิบาย

--- src/graphic.cpp
+++ src/graphic.cpp
@@ -6,7 +6,12 @@ GLabel       label;
 // ออกแบบกราฟิก
 void GRAPHIC::design(){
   wifi_manager.align(ALIGN_TOP_RIGHT,-10);
+
+#if UPLOAD_USB==1
   label = "ทดสอบ Upload ทางสาย USB";  // รอบแรกให้ใช้บรรทัดนี้ และเลือก Port แบบ Serail ports ที่เสียบ
-  //label = "ทดสอบ Upload ทาง OTA";   // รอบถัดๆไปให้สลับมาใช้บรรทัดนี้ แล้ว Upload แบบ OTA ด้วยการเลือก Port แบบ Network ports
+#else
+  label = "ทดสอบ Upload ทาง OTA";   // รอบถัดๆไปให้สลับมาใช้บรรทัดนี้ แล้ว Upload แบบ OTA ด้วยการเลือก Port แบบ Network ports
+#endif
+
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 /* ตัวอย่าง OTA ให้กำหนดเลือกบอร์ดที่ platformio.ini */
+/* ดูคำอธิบาย วิธี upload ทาง USB และ ทาง OTA ใน platformio.ini */
+
 
 #include <BlynkGOv3.h>
 #include "project.h"
@@ -13,11 +15,12 @@ void setup() {
 
 void loop() {
   BlynkGO.update();
-  BasicOTA::handle();
-  // WebServerOTA::handle();
+  BasicOTA::handle();           // OTA แบบ Basic
+  // WebServerOTA::handle();    // OTA แบบ WebServer
 }
 
 WIFI_CONNECTED(){
-  BasicOTA::begin(); 
-  //  WebServerOTA::begin();
+  BasicOTA::begin();            // OTA แบบ Basic
+  //  WebServerOTA::begin();    // OTA แบบ WebServer
 }
+
