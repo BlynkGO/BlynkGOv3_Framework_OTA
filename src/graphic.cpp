d7a66f3ca294c457a51a99129e3df273796fb333
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2024-04-03 16:11:39 +0700

    Update graphic.cpp

--- src/graphic.cpp
+++ src/graphic.cpp
@@ -8,9 +8,9 @@ void GRAPHIC::design(){
   wifi_manager.align(ALIGN_TOP_RIGHT,-10);
 
 #if UPLOAD_USB==1
-  label = "ทดสอบ Upload ทางสาย USB";  // รอบแรกให้ใช้บรรทัดนี้ และเลือก Port แบบ Serail ports ที่เสียบ
+  label = "ทดสอบ Upload ทางสาย USB";  // รอบแรก เลือกอัพโหลดแบบ USB โดยเลือก Port แบบ Serial ports ที่เสียบ ข้อความจะแสดงบรรทัดนี้
 #else
-  label = "ทดสอบ Upload ทาง OTA";   // รอบถัดๆไปให้สลับมาใช้บรรทัดนี้ แล้ว Upload แบบ OTA ด้วยการเลือก Port แบบ Network ports
+  label = "ทดสอบ Upload ทาง OTA";     // รอบถัดๆไปให้สลับมา อัพโหลดแบบ OTA โดยเลือก Port แบบ Network ports (IP Address)  ข้อความจะแสดงบรรทัดนี้
 #endif
 
 }
